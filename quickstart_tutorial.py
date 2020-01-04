0045f850c96be830c5af565592d0e85ec8004399
zhonghui.yao <zhonghua00700@gmail.com>
2020-01-04 23:01:42 +0800

    add sample_training

--- quickstart_tutorial.py
+++ quickstart_tutorial.py
@@ -21,3 +21,16 @@ print(a, type(a))
 a = a.reshape(3, 5)
 print(a, type(a))
 
+
+# Boolean masking
+import matplotlib.pyplot as plt
+
+a = np.linspace(0, 2 * np.pi, 50)
+b = np.sin(a)
+plt.plot(a, b)
+mask = b >= 0
+plt.plot(a[mask], b[mask], 'bo')
+mask = (b >= 0) & (a <= np.pi / 2)
+plt.plot(a[mask], b[mask], 'go')
+plt.show()
+
